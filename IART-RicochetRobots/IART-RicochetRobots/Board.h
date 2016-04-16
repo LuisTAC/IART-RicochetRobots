751333157e1ea3b8fb4d5cef18bef769a7c87f6c
Luis Costa <luis.torrescosta@gmail.com>
2016-04-16 03:57:45 +0100

    moveRobot implemented; Targets are now grouped in an array of vectors of targets for each robot; minDistanceToTarget & sumMinDistances implemented (to be used in heuristics).

--- IART-RicochetRobots/IART-RicochetRobots/Board.h
+++ IART-RicochetRobots/IART-RicochetRobots/Board.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 #include "Robot.h"
 #include "Wall.h"
@@ -14,10 +15,7 @@ class Board {
 private:
 	Robot robots[4]; //Blue, Green, Red, Yellow
 
-	vector <pair<int, int> > targetsBlue;
-	vector <pair<int, int> > targetsGreen;
-	vector <pair<int, int> > targetsRed;
-	vector <pair<int, int> > targetsYellow;
+	vector <pair<int, int> > targets[4];
 
 	vector<Wall> walls;
 
@@ -33,8 +31,10 @@ public:
 	vector <pair<int, int> > getTargetsRed();
 	vector <pair<int, int> > getTargetsYellow();
 	vector<Wall> getWalls();
+	int minDistanceToTarget(Color clr);
+	int sumMinDistances();
 
-	void moveRobot(Color c, Direction d);
+	void moveRobot(Color clr, Direction dir);
 };
 
 Board::Board(int type) {
@@ -46,25 +46,25 @@ Board::Board(int type) {
 		robots[2] = Robot(R, 11, 7);
 		robots[3] = Robot(Y, 0, 4);
 
-		targetsBlue.push_back(pair<int, int>(6, 5));
-		targetsBlue.push_back(pair<int, int>(12, 6));
-		targetsBlue.push_back(pair<int, int>(6, 10));
-		targetsBlue.push_back(pair<int, int>(13, 11));
+		targets[B].push_back(pair<int, int>(6, 5));
+		targets[B].push_back(pair<int, int>(12, 6));
+		targets[B].push_back(pair<int, int>(6, 10));
+		targets[B].push_back(pair<int, int>(13, 11));
 
-		targetsGreen.push_back(pair<int, int>(9, 1));
-		targetsGreen.push_back(pair<int, int>(1, 2));
-		targetsGreen.push_back(pair<int, int>(9, 13));
-		targetsGreen.push_back(pair<int, int>(3, 14));
+		targets[G].push_back(pair<int, int>(9, 1));
+		targets[G].push_back(pair<int, int>(1, 2));
+		targets[G].push_back(pair<int, int>(9, 13));
+		targets[G].push_back(pair<int, int>(3, 14));
 
-		targetsRed.push_back(pair<int, int>(10, 4));
-		targetsRed.push_back(pair<int, int>(3, 6));
-		targetsRed.push_back(pair<int, int>(1, 13));
-		targetsRed.push_back(pair<int, int>(14, 14));
+		targets[R].push_back(pair<int, int>(10, 4));
+		targets[R].push_back(pair<int, int>(3, 6));
+		targets[R].push_back(pair<int, int>(1, 13));
+		targets[R].push_back(pair<int, int>(14, 14));
 
-		targetsYellow.push_back(pair<int, int>(6, 1));
-		targetsYellow.push_back(pair<int, int>(14, 2));
-		targetsYellow.push_back(pair<int, int>(4, 9));
-		targetsYellow.push_back(pair<int, int>(8, 10));
+		targets[Y].push_back(pair<int, int>(6, 1));
+		targets[Y].push_back(pair<int, int>(14, 2));
+		targets[Y].push_back(pair<int, int>(4, 9));
+		targets[Y].push_back(pair<int, int>(8, 10));
 
 		walls.push_back(Wall(V, 4, 0));
 		walls.push_back(Wall(V, 10, 0));
@@ -148,7 +148,72 @@ vector <pair<int, int> > Board::getTargetsYellow() {
 vector<Wall> Board::getWalls() {
 	return walls;
 }
+int Board::minDistanceToTarget(Color clr) {
+	Robot robot = robots[clr];
+	pair<int, int> coordsRobot = robot.getCoords();
+	int min = 50;
+	for (unsigned int i = 0; i < targets[clr].size(); i++) {
+		pair<int, int> target = targets[clr][i];
+		int dist = (target.first - coordsRobot.first) + (target.second - coordsRobot.second);
+		if (dist < min) min = dist;
+	}
+	return min;
+}
+int Board::sumMinDistances() {
+	int total = 0;
+	for (int i = 0; i < 4; i++) {
+		total += minDistanceToTarget(Color(i));
+	}
+}
 
-void Board::moveRobot(Color c, Direction d) {
-
+void Board::moveRobot(Color clr, Direction dir) {
+	Robot robot = robots[clr];
+	pair<int, int> coordsRobot = robot.getCoords();
+	switch (dir)
+	{
+	case N:
+		if (coordsRobot.second == 0) return;
+		int max = 0;
+		for (unsigned int i = 0; i < walls.size(); i++) {
+			Wall wall = walls[i];
+			if (wall.getOrientation() == H && wall.getCoords().first == coordsRobot.first &&
+				wall.getCoords().second<coordsRobot.second && wall.getCoords().second>max)
+					max = wall.getCoords().second;
+		}
+		if (max != 0) robot.setCoords(coordsRobot.first, max);
+		break;
+	case E:
+		if (coordsRobot.first == 15) return;
+		int min = 15;
+		for (unsigned int i = 0; i < walls.size(); i++) {
+			Wall wall = walls[i];
+			if (wall.getOrientation() == V && wall.getCoords().second == coordsRobot.second &&
+				wall.getCoords().first>coordsRobot.first && wall.getCoords().first<min)
+				min = wall.getCoords().first;
+		}
+		if (min != 15) robot.setCoords(min, coordsRobot.second);
+		break;
+	case S:
+		if (coordsRobot.second == 15) return;
+		int min = 15;
+		for (unsigned int i = 0; i < walls.size(); i++) {
+			Wall wall = walls[i];
+			if (wall.getOrientation() == H && wall.getCoords().first == coordsRobot.first &&
+				wall.getCoords().second>coordsRobot.second && wall.getCoords().second<min)
+				min = wall.getCoords().second;
+		}
+		if (min != 15) robot.setCoords(coordsRobot.first, min);
+		break;
+	case W:
+		if (coordsRobot.first == 0) return;
+		int max = 0;
+		for (unsigned int i = 0; i < walls.size(); i++) {
+			Wall wall = walls[i];
+			if (wall.getOrientation() == V && wall.getCoords().second == coordsRobot.second &&
+				wall.getCoords().first<coordsRobot.first && wall.getCoords().first>max)
+				max = wall.getCoords().first;
+		}
+		if (max != 0) robot.setCoords(max, coordsRobot.second);
+		break;
+	}
 }
